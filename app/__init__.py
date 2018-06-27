2b1bc57b9b9584efde18211c6f8f04082dbed294
Hyakusenrenma <the1234@qq.com>
2018-06-27 17:39:37 +0800

    commit

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+from app import views, models
+from flask_openid import OpenID
+from config import basedir
+import os
+from flask_login import LoginManager
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app,os.path.join(basedir,'tmp'))
